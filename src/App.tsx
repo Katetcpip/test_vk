e0740d221cc6ae51af4838c6f06e3a81e17e7ae0
Katetcpip <katetcpip@yabdex.ru>
2023-05-09 23:53:53 +0300

    Проверки на ввод, кол-во участников, название встр

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,34 @@ import { notification, TimePicker, DatePicker} from 'antd';
 import {useState} from 'react'
 import './App.css'
 import dateFormat from "dateformat";
+import { Radio, Form, Input, Button, Select } from 'antd';
+
+const { Option } = Select;
 
 const App = () => {
 
-    const chooseTower = ['А', 'Б'];
+    const chooseTower : Array<string> = ['А', 'Б'];
     const chooseFloor : Array<number> = [];
     const chooseRoom : Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const timeIntervals : Array<string> = [
+        '09:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', 
+        '12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00', 
+        '15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00', 
+        '18:00 - 19:00', '19:00 - 20:00', '20:00 - 21:00'
+    ]
 
     for (let floor : number = 3; floor <= 27; floor++){
         chooseFloor.push(floor);
     }
 
+    const [name, setName] = useState('');
     const [tower, setTower] = useState('А');
     const [floor, setFloor] = useState('3');
-    const [room, setRoom] = useState('1');
+    const [room, setRoom] = useState(1);
     const [msg, setMsg] = useState('');
     const [date, setDate] = useState('');
-    const [timeFrom, setTimeFrom] = useState('');
-    const [timeTo, setTimeTo] = useState('');
+    const [time, setTime] = useState();
+    const [participants, setParticipants] = useState(1);
 
     const [api, contextHolder] = notification.useNotification();
     const openNotification : Function = () : void => {
@@ -32,95 +42,148 @@ const App = () => {
         });
       };
 
-    const onChanheData = (e:any) => {
-        e.preventDefault();
+    const onChangeData = () => {
         let dataReserv;
         if (date === '') {dataReserv = dateFormat(new Date() , 'dd mm yyyy')} else {dataReserv = date}
         const data = {
+            Name : name,
             Tower : tower,
             Floor: floor,
             Room: room,
             Data: dataReserv,
-            TimeFrom: timeFrom,
-            TimeTo: timeTo,
+            Time : time,
+            Participants: participants,
             Messsage: msg
         }
         console.log(JSON.stringify(data))
         openNotification()
+        // localStorage.setItem(tower, JSON.stringify(data))
+
     }
 
     const onChange = (date:any, dateString:any) => {
         setDate(dateString)
       };
 
-    const onChange2 = (e:any) => {
-        let timeFrom = e[0].$H + ':' + e[0].$m + ':' + e[0].$s       
-        let timeTo = e[1].$H + ':' + e[1].$m + ':' + e[1].$s
-        setTimeTo(timeTo);
-        setTimeFrom(timeFrom)
-      };
-
       const Clear = () => {
         setMsg('')
         setTower('А')
         setFloor('3')
-        setRoom('1')
+        setRoom(1)
+        setParticipants(1)
+        setName('')
       }
 
     return (
-        <div className='flex mainBlock'>
+        <div className='flex mainBlock pt-8 pb-8'>
             {contextHolder}
-            <form className='classForm md:w-1/3 w-10/12 z-50' onSubmit={e => onChanheData(e)}>
+            <Form className='classForm md:w-1/3 w-10/12 z-50' onFinish={() => onChangeData()} >
 
-                <div className="styleBlock">
-                    <label htmlFor="firstName" className='textForInput relative z-50'>Башня</label>
-                    <select className='inputStyle'
-                            onChange={e => setTower(e.target.value)}
-                    >{chooseTower.map( (el) =>
-                        <option className='text-gray-400 text-xl' key={el}>{el}</option> )
-                    }</select>
-                </div>
+            <label htmlFor="firstName" className='textForInput relative z-50'>Название встречи</label>
+            <Form.Item
+                name="nickname"
+                className="styleBlock"
+                rules={[{ required: true, message: 'Введите название встречи!', whitespace: true }]}
+            >
+                <Input className='inputStyle' onChange={(e) => setName(e.target.value)} placeholder={"Встреча..."} value={name}/>
+            </Form.Item>
 
-                <div className="styleBlock">
-                    <label htmlFor="firstName" className='textForInput relative z-50'>Этаж</label>
-                    <select className='inputStyle'
-                    onChange={e => setFloor(e.target.value)}
-                    >{chooseFloor.map( (el) =>
-                        <option className='text-gray-800 text-xl' key={el}>{el}</option> )
-                    }</select>
-                </div>
+            <label htmlFor="firstName" className='textForInput relative z-50'>Башня</label>
+            <Form.Item
+                name='tower'
+                className="styleBlock"
+                initialValue="A"
+                >
+                <Select className='!inputStyle rounded-xl' onChange={(e) => setTower(e)}>
+                {chooseTower.map( (el) =>
+                    <Option value={el} key={el}>{el}</Option> )
+                }
+                </Select>
+            </Form.Item>
 
-                <div className="styleBlock">
-                    <label htmlFor="email" className='textForInput'>Переговорка</label>
-                    <select className='inputStyle'
-                    onChange={e => setRoom(e.target.value)}
-                    >{chooseRoom.map( (el) =>
-                        <option className='text-gray-800 text-xl' key={el}>{el}</option> )
-                    }</select>
-                </div>
+            <label htmlFor="firstName" className='textForInput relative z-50'>Этаж</label>
+            <Form.Item
+                name='floor'
+                className="styleBlock"
+                initialValue="3"
+                >
+                <Select className='!inputStyle rounded-xl' onChange={(e) => setFloor(e)}>
+                {chooseFloor.map( (el) =>
+                    <Option value={el} key={el}>{el}</Option> )
+                }
+                </Select>
+            </Form.Item>
 
-                <div className="styleBlock flex !flex-row flex-wrap justify-between">
-                    <label htmlFor="email" className='textForInput w-full'>Дата и интервал времени</label>
-                    <DatePicker onChange={onChange} className='inputStyle sizePicker'/>
-                    <TimePicker.RangePicker  onChange={value => onChange2(value)} className='inputStyle sizePicker'/>
-                </div>
+            <label htmlFor="firstName" className='textForInput relative z-50'>Переговорка</label>
+            <Form.Item
+                name='room'
+                className="styleBlock"
+                initialValue="1"
+                >
+                <Select className='!inputStyle rounded-xl' onChange={(e) => setRoom(e)}>
+                {chooseRoom.map( (el) =>
+                    <Option value={el} key={el}>{el}</Option> )
+                }
+                </Select>
+            </Form.Item>
 
-                <div className="styleBlock">
-                    <label htmlFor="comment" className='textForInput'>Комментарий</label>
-                    <input 
-                        name="comment" 
-                        type="text" 
-                        className='inputStyle !pb-12' 
-                        value={msg}
-                        placeholder='Пожелания...'
-                        onChange={e => setMsg(e.target.value)}/>
-                </div>
+            <label htmlFor="email" className='textForInput w-full'>Дата и интервал времени</label>
+            <Form.Item className="styleBlock" 
+                        name="date_related_controls" 
+                        rules={[{ required: true, message: 'Выберите дату!' }]}>
+                <DatePicker placeholder='Выберите дату...' name="date-picker" onChange={onChange} className='inputStyle !w-full'/>
+            </Form.Item>
 
-                <div className="flex flex-row flex-wrap mt-8 gap-4">
-                    <button type="submit" className="btnBook hover:scale-95 transition hover:duration-500">Отправить</button>
-                    <button type="reset" onClick={Clear} className="btnClear hover:scale-95 transition hover:duration-500">Очистить</button>
-                </div>
-            </form>
+            <Form.Item
+                name='time_related_controls'
+                className="styleBlock"
+                rules={[{ required: true, message: 'Выберите время!' }]}
+                >
+                <Select onChange={(e) => setTime(e)} className='!inputStyle rounded-xl' placeholder={(new Date()).getHours() + ":"+ (String((new Date()).getMinutes()).length === 2 ? (new Date()).getMinutes() : "0" + (new Date()).getMinutes())}>
+                    {timeIntervals.map( (el) => {
+                        return (
+                            
+                        JSON.stringify(new Date()).substr(1, 10) == date 
+                        
+                        ? String((new Date()).getHours()) < (el[0] + el[1]) && <Option value={el} key={el}>{el}</Option>   
+
+                        : <Option value={el} key={el}>{el}</Option>)
+                    })}
+                </Select>
+            </Form.Item>
+
+            <label htmlFor="participants" className='textForInput w-full'>Количество участников</label>
+                <Radio.Group onChange={e => setParticipants(e.target.value)} value={participants} className='inputStyle'>
+                <Radio className='radioStyle' value={1}>1</Radio>
+                <Radio className='radioStyle' value={2}>2</Radio>
+                <Radio className='radioStyle' value={3}>3+</Radio>
+            </Radio.Group>
+
+            <div className="styleBlock">
+                <label htmlFor="comment" className='textForInput'>Комментарий</label>
+                <input 
+                    name="comment" 
+                    type="text" 
+                    className='inputStyle !pb-12' 
+                    value={msg}
+                    placeholder='Пожелания...'
+                    onChange={e => setMsg(e.target.value)}/>
+            </div>
+
+            <div className="flex flex-row flex-wrap mt-8 gap-4">
+                <button type="reset" onClick={() => Clear()} className="btnClear hover:scale-95 transition hover:duration-500">Очистить</button>
+                            
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" 
+                            className="!btnBook hover:scale-95 transition 
+                                hover:duration-500 !bg-white 
+                                !px-8 !p-2 h-full !text-violet-800 
+                                !rounded-md !font-semibold !text-center">
+                    Отправить
+                    </Button>
+                </Form.Item>
+            </div>
+            </Form>
 
             <div className='colorBlock md:top-80 md:left-96 top-44 left-10'></div>
             <div className='colorBlock2 md:bottom-44 md:right-96 bottom-20 right-20'></div>
